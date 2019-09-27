e70b6180935612fdca9f473513bb52c42e0096ec
ajulayfi <ajulayfi@gmail.com>
2019-09-27 04:14:09 -0500

    This is my first commit...

---
+++ CrapsSimulation/Test/MyTest.java
@@ -0,0 +1,26 @@
+
+
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+
+class MyTest
+{
+	 
+	@Test
+	 void Dietest1()
+	{
+		assertEquals("Die.testStatic()", Die.testStatic());
+		
+	}
+	@Test
+	 void CrookedDie1test2()
+	{
+		assertEquals("CrookedDie1.testStatic()", CrookedDie1.testStatic());
+	}
+
+
+}
