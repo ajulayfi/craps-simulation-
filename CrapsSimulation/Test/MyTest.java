01472ea31421337375f48502598fc90e84619462
ajulayfi <ajulayfi@gmail.com>
2019-09-27 04:54:45 -0500

    This is my second commit

--- CrapsSimulation/Test/MyTest.java
+++ CrapsSimulation/Test/MyTest.java
@@ -14,12 +14,14 @@ class MyTest
 	 void Dietest1()
 	{
 		assertEquals("Die.testStatic()", Die.testStatic());
+		Die test1 = new Die();
 		
 	}
 	@Test
 	 void CrookedDie1test2()
 	{
 		assertEquals("CrookedDie1.testStatic()", CrookedDie1.testStatic());
+		Die test2 = new CrookedDie1();
 	}
 
 
